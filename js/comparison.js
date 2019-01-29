df38ba05db1f2f1f5cafb497e817c3f5f6d7f703
Christine Cua <cloudycua@gmail.com>
2019-01-29 17:15:46 +0000

    Automatically backed up by Learn

--- js/comparison.js
+++ js/comparison.js
@@ -1,7 +1,6 @@
 'use strict';
 
 function testFalseLooseEqualityComparison(){
-<<<<<<< HEAD
     var x = 0;
     var y = 1;
     return x == y;
@@ -30,13 +29,3 @@ function testTrueStrictEqualityComparison(){
     var y = "1";
     return x === parseInt(y);
 }
-=======
-  var x = 0;
-  var y = 1;
-  if x == y {
-    return false;
-  } else {
-    return true;
-  }
-}
->>>>>>> ae761d6052b13069c3d140f83e7d4a6b030bca26
